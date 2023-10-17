092163cf037c3460e5cf6c91d3b90fbf2a9b7566
Anifu-ngugi <anifungugi1@gmail.com>
2023-10-17 16:47:18 -0700

    changes on flags and precision

--- get_flags.c
+++ get_flags.c
@@ -1,65 +1,35 @@
 #include "main.h"
 
 /**
-<<<<<<< HEAD
- * get_flags - function to calcaclute flags
- * @format: aguments to print the fommatted strings to calaculate
- * @i: argument that that takes a  i active
- * Return: returns a flags
- */
-int get_flags(const char *format, int *i)
-{
-	int flags = 0;
-	int i, cur_k;
-	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
-	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
-
-	for (cur_k = *i + 1; format[cur_k] != '\0'; cur_k++)
-	{
-		for (i = 0; FLAGS_CH[i] != '\0'; i++)
-			if (format[cur_k] == FLAGS_CH[i])
-			{
-				flags |= FLAGS_ARR[i];
-				break;
-			}
-
-		if (FLAGS_CH[i] == 0)
-			break;
-	}
-
-	*i = cur_k - 1;
-
-	return (flags);
-}
-
-=======
- * get_flags - Calculates the active flags
+ * get_flags - Calculates active flags
  * @format: Formatted string in which to print the arguments
- * @i: taking a parameter.
- *
+ * @i: take a parameter.
  * Return: Flags:
  */
 int get_flags(const char *format, int *i)
 {
-	int n, curr_j;
+	/* - + 0 # ' ' */
+	/* 1 2 4 8  16 */
+	int j, curr_i;
 	int flags = 0;
 	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
 	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
 
-	for (curr_j = *i + 1; format[curr_j] != '\0'; curr_j++)
+	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
 	{
-		for (n = 0; FLAGS_CH[n] != '\0'; n++)
-			if (format[curr_j] == FLAGS_CH[n])
+		for (j = 0; FLAGS_CH[j] != '\0'; j++)
+			if (format[curr_i] == FLAGS_CH[j])
 			{
-				flags |= FLAGS_ARR[n];
+				flags |= FLAGS_ARR[j];
 				break;
 			}
-			if (FLAGS_CH[n] == 0)
+
+		if (FLAGS_CH[j] == 0)
 			break;
 	}
 
-	*i = curr_j - 1;
+	*i = curr_i - 1;
 
 	return (flags);
 }
->>>>>>> 674f6f738b65e24bd9e79645c3e0c569cbcf57f1
+
--- get_precision.c
+++ get_precision.c
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-#include "main.h"
-
-/**
- * get_precision - fuction that calculates the presion
- * @format: arguments that prints the formatted string
- * @i: arguments where a list of string to be printed
- * @list: arguments where a lsit of arguments is printed
- *
- * Return: returns int precision
- */
-int get_precision(const char *format, int *i, va_list list)
-{
-	int precision = -1;
-	int cur_k = *i + 1;
-
-	if (format[cur_k] != '.')
-		return (precision);
-
-	precision = 0;
-
-	for (cur_k += 1; format[cur_k] != '\0'; cur_k++)
-	{
-		if (is_digit(format[cur_k]))
-		{
-			precision *= 10;
-			precision += format[cur_k] - '0';
-		}
-		else if (format[cur_k] == '*')
-		{
-			cur_k++;
-			precision = va_arg(list, int);
-=======
-
 #include "main.h"
 
 /**
@@ -63,22 +29,14 @@ int get_precision(const char *format, int *i, va_list list)
 		{
 			curr_j++;
 			preci = va_arg(list, int);
->>>>>>> 674f6f738b65e24bd9e79645c3e0c569cbcf57f1
 			break;
 		}
 		else
 			break;
 	}
 
-<<<<<<< HEAD
-	*i = cur_k - 1;
 
-	return (precision);
-}
-
-=======
-	*i = curr_j - 1;
+	*i = cur_j - 1;
 
 	return (preci);
 }
->>>>>>> 674f6f738b65e24bd9e79645c3e0c569cbcf57f1
